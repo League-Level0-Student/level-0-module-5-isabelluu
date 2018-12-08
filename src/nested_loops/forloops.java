c2789e0141dc4dcbb29c5175e6832264dd0fed7d
isabelluu <league@WTS-IM-17.attlocal.net>
2018-12-08 14:27:55 -0800

    For loops

---
+++ src/nested_loops/forloops.java
@@ -0,0 +1,65 @@
+package nested_loops;
+
+public class forloops 
+{
+public static void main(String[] args) {
+	/*for(int z = 1; z<101; z++)
+		System.out.println(z);
+	
+	for(int a = 100; a>0; a--)
+		System.out.println(a);
+	
+	for(int b = 2; b<101; b+=2)
+		System.out.println(b);
+	
+	for(int c = 1; c<100; c+=2)
+		System.out.println(c);
+	
+	for (int d = 1; d<501; d++)
+		if(d%2==0)
+			System.out.println(d + " is even.");
+		else 
+			System.out.println(d + " is odd.");
+	
+	for(int e = 1; e<777; e++)
+		if(e%7 == 0)
+			System.out.println(e);
+	
+	int g = 2003;
+	for(int f = 0; f<16; f++)
+			System.out.println("In " + g++ + ", I was " + f + " years old."); 
+	
+	for (int h = 0; h<3; h++)
+		for(int i = 0; i<3; i++)
+			System.out.println(h + " " +i);
+	
+	for(int a = 1; a<8; a+=3)
+	{
+		int b = a+1;
+		int c = a+2;
+				System.out.println(a + " " + b +" " + c);
+	}
+   for(int k=1; k<101; k+=10)
+   {
+	int a = k+1;
+   	int b = k+2;
+   	int c = k+3;
+   	int d = k+4;
+   	int e = k+5;
+   	int f = k+6;
+   	int g = k+7;
+   	int h = k+8;
+   	int i = k+9;
+    		System.out.println(k + " " + a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + i);
+  
+   }*/
+	
+
+	for(int column = 0; column<=10; column++)
+		for(int row = 0; row<=10; row++)
+				System.out.println("*  ");
+		
+	
+
+}
+}
