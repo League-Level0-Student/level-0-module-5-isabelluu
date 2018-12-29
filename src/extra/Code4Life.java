cd6c5d0255f0d2b41b053b450314af5a5caf40d2
Ddubby <league@WTS-IM-17.attlocal.net>
2018-12-29 14:29:08 -0800

    Day 9

--- src/extra/Code4Life.java
+++ src/extra/Code4Life.java
@@ -11,18 +11,36 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
-
+	
+public static void main(String[] args) {
 	// 1. Ask the user how many hours they spent coding this week.
+		String hours = JOptionPane.showInputDialog("How many hours have you spent coding this week?");
 
+		int hour = Integer.parseInt(hours);
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
 
+		if(hour<=2)
+		{
+			JOptionPane.showMessageDialog(null, "Stop watching youtube and write code instead.");
+		}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+		else if(3<=hour && hour<=5)
+		{
+			JOptionPane.showMessageDialog(null, "You are a code ninja!");
+		}
+			
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
+		if(hour>5)
+		{
+			playBatmanTheme();
+		}
+}
+
 
 	private static void playBatmanTheme() {
 		try {
--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -10,20 +10,36 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+		Robot Koa = new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
+Koa.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "What color would you like?", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Blue", "Yellow"}, 0);
 		
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
 		
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String polygons = JOptionPane.showInputDialog("How man polygons do you want drawn?");
+		int polygon = Integer.parseInt(polygons);
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		for(int i = polygon; i>0; i--)
+		{
+			if(colorChoice == 0)
+				Koa.setPenColor(Color.RED);
+			else if(colorChoice == 1)
+				Koa.setPenColor(Color.cyan);
+			if(colorChoice == 2)
+				Koa.setPenColor(Color.YELLOW);
+
+		for(int x = 1; x<7; x++)
+		{
+		Koa.penDown();
+		Koa.move(100);
+		Koa.turn(60);
+		}
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
+		}
 	}
-}
+	}
 
